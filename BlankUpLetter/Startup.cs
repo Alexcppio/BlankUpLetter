2ba6e0f5b85292116ce0df1b9306d7c2565eae3d
Alexcppio <test21112019test@gmail.com>
2022-01-07 17:57:53 +0500

    learning

--- BlankUpLetter/Startup.cs
+++ BlankUpLetter/Startup.cs
@@ -12,25 +12,43 @@ namespace BlankUpLetter
 {
     public class Startup
     {
+        IWebHostEnvironment _env;
+        public Startup(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            int x = 5;
+            int y = 8;
+            int z = 0;
+            app.Use(async (context, next) =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                z = x * y;
+                await next.Invoke();
             });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"x * y = {z}");
+            });
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync($"Application name: {_env.ApplicationName}\nApplication RootPath:{_env.ContentRootPath}\nApplication Environment: {_env.EnvironmentName}\nApplication WebRootFileProvider: {_env.WebRootFileProvider}");
+            //    });
+            //});
         }
     }
 }
